04cbc7630e16ff0ccf44f4d398f09234b8d33bd8
kpicaza <kpicaza.sound@gmail.com>
2017-03-19 17:55:35 +0100

    cover with tests quoteresponder

--- packages/quotes/spec/Inspiration/Quotes/Infrastructure/Forismatic/QuoteResponderSpec.php
+++ packages/quotes/spec/Inspiration/Quotes/Infrastructure/Forismatic/QuoteResponderSpec.php
@@ -3,10 +3,13 @@
 namespace spec\Inspiration\Quotes\Infrastructure\Forismatic;
 
 use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Stream;
+use Inspiration\Quotes\DomainModel\QuoteResponseException;
 use Inspiration\Quotes\Infrastructure\Forismatic\QuoteRequest;
 use Inspiration\Quotes\Infrastructure\Forismatic\QuoteResponder;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Prophecy\Prophet;
 
 class QuoteResponderSpec extends ObjectBehavior
 {
@@ -14,14 +17,36 @@ class QuoteResponderSpec extends ObjectBehavior
     const AUTHOR = 'Annonimous';
     const LINK = 'http://link.com';
 
+    function it_should_thrown_an_exception_with_empty_response()
+    {
+        $this->shouldThrow(QuoteResponseException::class)->during('getResponse', [new Response()]);
+    }
+
+    function it_should_thrown_an_exception_with_invalid_response()
+    {
+        $response = new Response();
+        $response = $response->withStatus(400);
+
+        $this->shouldThrow(QuoteResponseException::class)->during('getResponse', [$response]);
+    }
+
     function it_should_transform_forismatic_response_into_md_quote()
     {
-        $response = $this->getResponse(
-            new Response()
+        $prophet = new Prophet();
+        $response = $prophet->prophesize(Response::class);
+        $response->getStatusCode()->willReturn(200);
+        $response->getBody()->willReturn(
+            json_encode([
+                'quoteText' => self::TEXT,
+                'quoteAuthor' => self::AUTHOR,
+                'quoteLink' => self::LINK,
+            ], true)
         );
 
-        $response['text']->shouldBe(self::TEXT);
-        $response['author']->shouldBe(self::AUTHOR);
-        $response['link']->shouldBe(self::LINK);
+        $body = $this->getResponse($response);
+
+        $body['text']->shouldBe(self::TEXT);
+        $body['author']->shouldBe(self::AUTHOR);
+        $body['link']->shouldBe(self::LINK);
     }
 }
