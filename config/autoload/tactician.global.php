6de9d1d856ae7d992fe16d16de8d3c436f34b09c
kpicaza <kpicaza.sound@gmail.com>
2017-04-22 13:17:19 +0200

    add dependencies to project and add command handler relation in tactician config

--- config/autoload/tactician.global.php
+++ config/autoload/tactician.global.php
@@ -3,10 +3,24 @@
 return [
     'dependencies' => [
         'invokables' => [
-
+            Kpicaza\Inspiration\Factory\RequestCommandFactory::class =>
+                Kpicaza\Inspiration\Factory\RequestCommandFactory::class,
+            \Inspiration\Questions\DomainModel\QuestionRequestHandler::class =>
+                \Inspiration\Questions\DomainModel\QuestionRequestHandler::class
         ],
         'factories' => [
-
+            \League\Tactician\Container\ContainerLocator::class =>
+                \Kpicaza\Inspiration\Factory\ContainerLocatorFactory::class
         ]
-    ]
+    ],
+    'middleware' => [
+        League\Tactician\Plugins\LockingMiddleware::class => 500,
+    ],
+    'tactician' => [
+        'default-locator' => \League\Tactician\Container\ContainerLocator::class,
+        'handler-map' => [
+            Inspiration\Questions\DomainModel\QuestionRequestCommand::class =>
+                Inspiration\Questions\DomainModel\QuestionRequestHandler::class,
+        ],
+    ],
 ];
